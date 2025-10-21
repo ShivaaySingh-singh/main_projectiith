704ff435c8f6681c370ec4566c1d82bc5c626da4
ShivaaySingh-singh <shivamsingh11012018@gmail.com>
2025-10-21 17:09:15 +0530

    Update settings.py
    email config removed

--- project_portal/settings.py
+++ project_portal/settings.py
@@ -145,8 +145,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "shivamsingh11012018@gmail.com"
-EMAIL_HOST_PASSWORD = "zsha tbmo waot xvop"
+EMAIL_HOST_USER = 
+EMAIL_HOST_PASSWORD = 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
